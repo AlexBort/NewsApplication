eb07cc7526f7be18e767ecde415dd8dfc6ec7f90
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2021-07-30 18:33:35 +0300

    migration of libs to  Androidx; added necessary dependencies to use JetpackCompose

--- app/src/main/java/com/example/newsapplication/adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/example/newsapplication/adapters/ViewPagerAdapter.java
@@ -1,9 +1,9 @@
 package com.example.newsapplication.adapters;
 
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 import com.example.newsapplication.fragments.FavouritesFragment;
 import com.example.newsapplication.fragments.StoriesFragment;
